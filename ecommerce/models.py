00845d78770d22b4e5d97124ed7aa9da05a281cb
Samuel Fendell <sfendell91@gmail.com>
2013-03-03 18:23:27 -0800

    Added all of the fields for product model.

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -8,7 +8,27 @@ Created on Mar 3, 2013
 from django.db import models
 
 class ProductModel(models.Model):
-    productName = models.CharField(max_length=128)
+    productName = models.CharField()
     productDesc = models.TextField()
     supplierId  = models.IntegerField()
+    supplierProductCode = models.CharField()
+    supplierPrice = models.DecimalField(decimal_places = 2)
+    merchantPrice = models.DecimalField(decimal_places = 2)
+    custFactor = models.FloatField(blank=False) 
+    manufacturerName = models.CharField()
+    
+    createdDate = models.DateTimeField()
+    updatedDate = models.DateTimeField()
+    lastsoldDate = models.DateTimeField()
+    
+    numSold = models.PositiveIntegerField()
+    numReturned = models.PositiveIntegerField()
+    
+    published = models.BooleanField()
+    categorized = models.BooleanField()
+    
+    brandId = models.PositiveIntegerField()
+    
+    availibility = models.CharField()
+    
     
