83dc4ddb30fa165b5e347e2ccc789975dc38c85b
Samuel Fendell <sfendell91@gmail.com>
2013-03-03 18:38:38 -0800

    Initial supplier and brand models added.

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -6,10 +6,28 @@ Created on Mar 3, 2013
 
 
 from django.db import models
+import django.contrib.localflavor.us.forms as localflavor
+
+class BrandModel(models.Model):
+    brandName = models.CharField()
+    
+class SupplierModel(models.Model):
+    name = models.CharField()
+    desc = models.TextField()
+    merchantCustomerId = models.CharField()
+    
+    primaryEmail = models.EmailField()
+    secondEmail = models.EmailField()
+    phone = localflavor.USPhoneNumberField()
+    fax   = localflavor.USPhoneNumberField()
+    address = models.CharField()
+    city = models.CharField()
+    state = localflavor.USStateField()
+    zip = localflavor.USZipCodeField()
 
 class ProductModel(models.Model):
-    productName = models.CharField()
-    productDesc = models.TextField()
+    name = models.CharField()
+    desc = models.TextField()
     supplierId  = models.IntegerField()
     supplierProductCode = models.CharField()
     supplierPrice = models.DecimalField(decimal_places = 2)
